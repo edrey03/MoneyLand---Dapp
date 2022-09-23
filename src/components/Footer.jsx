f56b6c437a21ec3c34afebed4964e6f0f64d2d04
edrey03 <113221389+edrey03@users.noreply.github.com>
2022-09-22 23:42:38 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <>
         <footer>
-            <p className="copy">All Rights Reserved - Inifinity Block&copy;</p>
+            <p className="copy">All Rights Reserved - Money Land&copy;</p>
         </footer>
         </>
     );
